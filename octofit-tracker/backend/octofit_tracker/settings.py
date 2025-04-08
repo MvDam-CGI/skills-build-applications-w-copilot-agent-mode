e20f4bcd2d88080f2150c1994bda2c957de4617a
MvDam-CGI <m.van.dam@cgi.com>
2025-04-08 13:50:27 +0000

    fix

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Djongo DB
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
